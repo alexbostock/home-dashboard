e55a60380d609ac18add2025aa61552935395e2c
alexbostock <alex@alexbostock.co.uk>
2019-12-08 17:59:57 +0000

    Make config state immutable with Immutable.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { List, Map } from 'immutable';
+import { serialize, deserialize } from 'json-immutable';
+
 import './App.css';
 import TrainTimes from './trains/TrainTimes';
 
@@ -21,27 +24,27 @@ class App extends React.Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return nextState.data === this.state.data;
+    return !(nextState.data.equals(this.state.data));
   }
 
   renderWidget(config) {
-    switch (config.type) {
+    switch (config.get('type')) {
       case 'live-trains':
         return (
           <TrainTimes
             key={config}
-            station={config.station}
-            arrivals={config.arrivals}
+            station={config.get('station')}
+            arrivals={config.get('arrivals')}
           />
         );
       default:
-        console.error(`Invalid widget type: ${config.type}`);
+        console.error(`Invalid widget type: ${config.get('type')}`);
         return null;
     }
   }
 
   render() {
-    const widgets = this.state.data.widgets
+    const widgets = this.state.data.get('widgets')
       .map(this.renderWidget);
 
     return (<div className="App">{widgets}</div>);
@@ -49,25 +52,26 @@ class App extends React.Component {
 }
 
 function defaultState() {
-  return {
-    widgets: [
-      {
+  return Map({
+    widgets: List([
+      Map({
         type: 'live-trains',
         station: 'STP',
         arrivals: false,
-      },
-    ],
-  };
+      }),
+    ]),
+  });
 }
 
 function loadSavedState() {
   const state = localStorage.getItem('config');
-  return state ? JSON.parse(state) : defaultState();
+
+  return state ? deserialize(state) : defaultState();
 }
 
 function saveState(state) {
   try {
-    localStorage.setItem('config', JSON.stringify(state));
+    localStorage.setItem('config', serialize(state));
     return true;
   } catch (err) {
     console.err('Saving to local storage failed.');
