fc3ee35a3a29c8df289d82cae886be3799d57429
alexbostock <alex@alexbostock.co.uk>
2019-12-14 22:57:39 +0000

    Fix edge case of sorting train times chronologically around midnight

--- src/trains/Services.js
+++ src/trains/Services.js
@@ -8,7 +8,7 @@ function Services(props) {
   const numServicesToRender = props.numServices || 3;
 
   const services = props.trains.services
-    .sort((a, b) => a.realTime - b.realTime)
+    .sort((a, b) => compareTimes(a.realTime, b.realTime))
     .slice(0, numServicesToRender)
     .map(renderService);
 
@@ -71,6 +71,21 @@ function ontimenessMessage(scheduled, actual) {
 function leftpad(num) {
   num = num.toString();
   return new Array(4 - num.length).fill('0').join('') + num;
+}
+
+// When comparing services running the same night but different sides of
+// midnight, chronological order is not the same as comparison of times.
+// This assmues that, if one train is expected between 1800 and 2359, and
+// another is expected between 0000 and 0559, the first is sooner.
+function compareTimes(t1, t2) {
+  const smaller = Math.min(t1, t2);
+  const larger = Math.max(t1, t2);
+
+  if (0 <= smaller && smaller < 600 && 1800 < larger && larger <= 2359) {
+    return t2 - t1;
+  } else {
+    return t1 - t2;
+  }
 }
 
 export default Services;
