fb70fb737eeb9ab189ac718349585de2944684a2
Alex Bostock <alex@bostock.uk>
2019-12-16 10:32:15 +0000

    Fix trying to load a non-existent theme stylesheet when config is not set

--- src/Theme.js
+++ src/Theme.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Theme(props) {
-  if (props.themeKey === 'light') {
+  if (!props.themeKey || props.themeKey === 'light') {
     return null;
   } else {
     const url = `${process.env.PUBLIC_URL}/themes/${props.themeKey}.css`;
